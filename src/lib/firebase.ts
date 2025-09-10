55f5d9803d5156810b9674a03e64a283c8a05c4f
3D Yuri Barbosa <byuri991@gmail.com>
2025-09-10 23:10:22 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFH8O36DKxdui3CClrJqkgEf_2YgsTOT0",
+  authDomain: "pecus-intermediate.firebaseapp.com",
+  databaseURL: "https://pecus-intermediate-default-rtdb.firebaseio.com",
+  projectId: "pecus-intermediate",
+  storageBucket: "pecus-intermediate.appspot.com",
+  messagingSenderId: "667966942508",
+  appId: "1:667966942508:web:df881058a5e69464175089"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
