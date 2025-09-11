157235f8f0c6c4399850e969ebd17e881249db29
3D Yuri Barbosa <byuri991@gmail.com>
2025-09-11 19:41:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -26,10 +26,11 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     // This is required to allow the Next.js dev server to accept requests from the browser
-    allowedDevOrigins: [
-      'https://*.cloudworkstations.dev',
-    ],
+    // In some Next.js versions, this is a top-level property.
   },
+  allowedDevOrigins: [
+    'https://*.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
