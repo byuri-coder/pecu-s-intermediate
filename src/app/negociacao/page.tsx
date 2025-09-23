f650b3fda39a6a785109be24ad9c145f61abfdaf
3D Yuri Barbosa <byuri991@gmail.com>
2025-09-23 01:13:31 +0000

    volte a bola com foto de perfil e as mensagens e suas funcionalidades

--- src/app/negociacao/page.tsx
+++ src/app/negociacao/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { ChatList } from './chat-list';
 import { MessageSquare } from 'lucide-react';
