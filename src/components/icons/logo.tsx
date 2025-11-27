3de64311d331e1b126ca44e7293cb2ba7326562c
3D Yuri Barbosa <byuri991@gmail.com>
2025-11-27 01:38:38 +0000

    não esta aparecendo a logo ``logo.png`` nos seus locais devidos

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,26 +1,18 @@
-
 "use client";
 
 import * as React from "react";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 
-export const Logo = (props: React.SVGProps<SVGSVGElement>) => {
+export const Logo = (props: React.ComponentProps<typeof Image>) => {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={cn("h-6 w-6", props.className)}
-            {...props}
-        >
-            <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2Z" fill="hsl(var(--primary))" stroke="none" />
-            <path d="M12 7v10" stroke="hsl(var(--primary-foreground))" />
-            <path d="M12 7a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5z" fill="hsl(var(--primary-foreground))" stroke="none"/>
-        </svg>
+        <Image
+            src="https://placehold.co/64x64/1e3a4b/ffffff?text=P"
+            alt="PECU'S INTERMEDIATE Logo"
+            width={32}
+            height={32}
+            className={cn("h-8 w-8", props.className)}
+        />
     );
 };
 
