d6fc9c65666c35db2dc87579db9a4e54b97f3446
3D Yuri Barbosa <byuri991@gmail.com>
2025-11-27 01:39:52 +0000

    mude ``logo.tsx`` para ``logo.png``

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -7,11 +7,12 @@ import { cn } from "@/lib/utils";
 export const Logo = (props: React.ComponentProps<typeof Image>) => {
     return (
         <Image
-            src="https://placehold.co/64x64/1e3a4b/ffffff?text=P"
+            src="/logo.png"
             alt="PECU'S INTERMEDIATE Logo"
             width={32}
             height={32}
             className={cn("h-8 w-8", props.className)}
+            {...props}
         />
     );
 };
